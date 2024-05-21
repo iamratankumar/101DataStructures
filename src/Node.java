abc8d94169d4ab89196ed4072a6bb0118b10b0b0
iamratankumar <iamratankumar@yahoo.com>
2024-05-21 00:28:22 -0500

    Added SinglyLinkedList implementation code

---
+++ src/Node.java
@@ -0,0 +1,13 @@
+public class Node {
+    int item;
+    Node next;
+
+
+    public Node(int item){
+        this.item = item;
+    }
+    public  Node(int item, Node next){
+        this.item = item;
+        this.next = next;
+    }
+}
---
+++ src/SinglyLinkedList.java
@@ -0,0 +1,49 @@
+public class SinglyLinkedList {
+    Node head = null;
+
+    public void insert(int x){
+        Node newNode = new Node(x);
+        if(head == null){
+            head = newNode;
+        }else{
+            Node current = head;
+            while (current.next != null){
+                current =current.next;
+            }
+            current.next = newNode;
+        }
+    }
+    public void insert(int index, int value){
+        Node newNode = new Node(value);
+        if(index == 0 && head == null){
+            head = newNode;
+        }else {
+            Node prev = head;//1,2,3,4  => 3
+            for (int i = 1; i < index; i++) {
+                prev = prev.next;
+            }
+            newNode.next = prev.next;
+            prev.next = newNode;
+        }
+
+    }
+
+    public void remove(int index){
+        if(head == null) throw new NullPointerException("List is Empty");
+        if(index == 0)  head = head.next;
+        Node curr = head;
+        for(int i=1; i<index;i++){
+            curr = curr.next;
+        }
+        if (curr == null) throw new IndexOutOfBoundsException();
+        curr.next = curr.next.next;
+    }
+    public void traverse(){
+        Node curr = head;
+        while (curr != null){
+            System.out.print(curr.item+" ");
+            curr = curr.next;
+        }
+    }
+
+}
