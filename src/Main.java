a35286df78c510480ab9ecfdea9ba6f99822bc48
Ratan Kumar <iamratankumar@yahoo.com>
2024-05-21 00:31:07 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+
+        for(int i =1; i<=10;i++){
+            linkedList.insert(i);
+        }
+        linkedList.insert(5,15);
+
+        linkedList.remove(1);
+        linkedList.traverse();
+
     }
 }
